26a819b9dd335e55a7142a9f30aee7bfe9bfb78e
Santiago <auto.sun.731@gmail.com>
2019-06-11 20:54:23 -0300

    primeira parte do backend da application

---
+++ backend/src/controllers/PostController.js
@@ -0,0 +1,9 @@
+const Post = require('../models/Post');
+
+module.exports({
+    async index(req, res){
+
+    },
+
+    async Store(req, res)
+})
---
+++ backend/src/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const routes = new express.Router();
+
+const PostController
+
+routes.post('/posts', );
+
+//POST http://localhost:3333/posts 
+
+module.exports = routes;
